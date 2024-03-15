b6f837adec55966b5e14f593fb576d2e439cbdff
ManuelGordillo <chefmanuelgordillo.m@gmail.com>
2024-03-15 17:16:09 -0500

    15 marzo variables valores

---
+++ Mi Avance/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("manuel");
+console.log(23);
+
+let firstName = "Franchesca";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let manuel_Gordillo = "MG";
+let $function = 27;
+
+let person = "Manuel";
+let PI = 3.1416;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+
+let country = "Peru";
+let continent = "South America";
+let population = "10 millon of persons";
+
+console.log(country);
+console.log(continent);
+console.log(population);
