fb23bbae6f42c9a2777b7bb3b130f4aa48c2d447
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2024-06-15 16:41:47 -0500

    Data Types

--- Mi Avance/01-Fundamentals-Part-1/starter/script.js
+++ Mi Avance/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("manuel");
@@ -10,6 +10,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//Variable name conventions
 let manuel_Gordillo = "MG";
 let $function = 27;
 
@@ -31,3 +32,36 @@ let population = "10 millon of persons";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Manuel");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Gordillo";
+console.log(lastName);
